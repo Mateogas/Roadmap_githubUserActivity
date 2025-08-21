0076d389f4792d6f821de052ba02bc6010bf27c9
Mateogas <dckarlmat@gmail.com>
2025-08-21 16:27:46 +0800

    Update on api fetching

--- githubUserActivity.java
+++ githubUserActivity.java
@@ -1,4 +1,10 @@
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.Scanner;
+import javax.json.Json;
+import javax.json.JsonArray;
+import javax.json.JsonReader;
+import java.io.StringReader;
 
 public class githubUserActivity{
 
@@ -10,6 +16,34 @@ public class githubUserActivity{
        System.out.println(name);
        String githublink = "https://api.github.com/users/" +name+"/events";
        System.out.println(githublink);
-       sc.close();
+       try {
+           URL url = new URL(githublink);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.connect();
+
+            int respCode = conn.getResponseCode();
+
+            if(respCode != 200){
+                throw new RuntimeException("Http response:"+respCode);
+
+            }else{
+                StringBuilder informationString = new StringBuilder();
+                Scanner scanner = new Scanner(url.openStream());
+                while(scanner.hasNext()){
+                    informationString.append(scanner.nextLine());
+
+
+                }
+                scanner.close();
+
+                System.out.println(informationString);
+                
+            }
+           // You can use 'url' here for further processing
+       } catch (Exception e) {
+           System.out.println("Invalid URL: " + e.getMessage());
+       }
+
     }
 }
